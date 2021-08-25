5914e3ca2ce1f32acc1bd73da7e2f21217fe295b
Hannah <hbrooks92@protonmail.com>
2021-08-25 16:10:29 -0700

    update comments

--- python/gpu-usage.py
+++ python/gpu-usage.py
@@ -1,6 +1,8 @@
 #!/bin/python
 import subprocess
 
+# NOTE: nvidia-smi, to the extent of my knowledge, only works with the proprietary NVIDIA drivers. If you are using the open-source Noveau drivers, this script will not function.
+
 used_encoded = subprocess.check_output(["nvidia-smi", "--query-gpu=memory.used", "--format=csv,noheader,nounits","|","tr","'\n'","' '"]) # the amount of memory being used in MiB
 total_encoded = subprocess.check_output(["nvidia-smi", "--query-gpu=memory.total", "--format=csv,noheader,nounits"]) # the total amount of memory in MiB
 
--- python/printcsv.py
+++ python/printcsv.py
@@ -2,6 +2,7 @@
 from sys import argv
 from pandas import read_csv
 
+# NOTE: Given that this is just a simple loop to process CSV files, there is no error handling other than from the python interpereter itself.
 for i in range (1,len(argv)):
     file=argv[i]
     table=read_csv(file)
