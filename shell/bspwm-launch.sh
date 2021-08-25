3643604dd154c778f1812c9a21060dc5ecf7936d
Hannah <hbrooks92@protonmail.com>
2021-08-25 14:04:49 -0700

    add comments

--- shell/bspwm-launch.sh
+++ shell/bspwm-launch.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # NOTE: My polybar is named "mybar". If your bar has a different name, substitute the name "mybar" for the name of your polybar.
+
 if pgrep polybar > /dev/null
 then
 	kill polybar && polybar mybar
