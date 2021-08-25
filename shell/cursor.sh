bf35b6364ff6cf9c477a6c789b915b93d5abecb6
Hannah <hbrooks92@protonmail.com>
2021-08-25 14:03:30 -0700

    add comments

--- shell/cursor.sh
+++ shell/cursor.sh
@@ -5,9 +5,9 @@ oldstty=$(stty -g)
 stty raw -echo min 0
 tput u7 > /dev/tty
 sleep 0.01
-IFS=';' read -r row col
+IFS=';'; read -r row col
 stty $oldstty
 
 row=$(expr $(expr substr $row 3 99) - 1)
 
-printf $row
+printf $row # This ONLY prints the current row in your terminal emulator or tty.
